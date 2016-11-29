e5b391d85868c201c3b951b979e7b78dcfc90a08
sidneymbell <sidneymb@uw.edu>
2016-11-28 17:15:01 -0800

    GARD renaming.

---
+++ scripts/gard_format.py
@@ -0,0 +1,18 @@
+from glob import glob
+from Bio import SeqIO
+
+infiles = glob('/Users/Sidney/nextstrain/dengue/data/subsampled_alignments/denv*.fasta')
+
+for f in infiles:
+    ofile = open(f.split('/')[-1].split('.')[0]+'_gard.fasta', 'w')
+    seqs = []
+    for s in SeqIO.parse(f, 'fasta'):
+        if len(s.description.split(' ')) < 2 or len(s.description.split('|')) < 2:
+            print s.description
+            continue
+        else:
+            strain = s.description.split(' ')[0]
+            acc = s.description.split('|')[1]
+            ofile.write('>'+strain+'/'+ acc + '\n' +str(s.seq)+'\n')
+    #         seqs.append(s)
+    # SeqIO.write(seqs, ofile_name, 'fasta')
