46d9b3d958c2ec5e7515adc84343c8e8b1d76ed9
sidneymbell <sidneymb@uw.edu>
2018-03-20 11:51:36 -0700

    Add layer to output nesting/organization for my own sanity

--- frequencies/sbatch_param_wrapper.py
+++ frequencies/sbatch_param_wrapper.py
@@ -1,4 +1,6 @@
 import os
+os.system("module load python2")
+os.system("pip install --user --upgrade matplotlib") # hack...... :)
 
 # regions = ['southeast_asia', 'south_america']
 region = 'southeast_asia'
@@ -12,9 +14,9 @@ for n in N:
 		for clade_resolution in clade_resolutions:
 			frequency_path = './%s/%s/%s_%s_frequencies.csv'%(region, clade_resolution, region, clade_resolution)
 			titer_path = './%s_Dij.csv'%antigenic_resolution
-			out_path = './%s/%s/'%(region, clade_resolution)
+			out_path = './%s/%s/%s/'%(region, clade_resolution, antigenic_resolution)
 			name = '%s_%s_%s_N%d'%(region, clade_resolution, antigenic_resolution, n)
-			cmd = 'python ./antigenic_fitness.py --frequency_path %s --titer_path %s --out_path %s --name %s --years_back %d --years_forward 1 --beta 0 5 --gamma 0 5 --sigma 0 5 --n_param_vals 10'%(frequency_path, titer_path, out_path, name, n)
+			cmd = 'python ./antigenic_fitness.py --frequency_path %s --titer_path %s --out_path %s --name %s --years_back %d --years_forward 1 --beta 0 10 --gamma 0 2 --sigma 0 5 --n_param_vals 10 --plot --save'%(frequency_path, titer_path, out_path, name, n)
 			print cmd
 			os.system("sbatch --wrap='%s'"%cmd)
 
