9f732f0a1a050b91133b5bc6e6fbcb05ba13dfd1
sidneymbell <sidneymb@uw.edu>
2018-04-13 14:40:25 -0700

    Simplify sbatch wrapper

--- frequencies/sbatch_param_wrapper.py
+++ frequencies/sbatch_param_wrapper.py
@@ -1,23 +1,16 @@
 import os
-# os.system("module load python2")
-# os.system("pip install --user --upgrade matplotlib") # hack...... :)
 
-# regions = ['southeast_asia', 'south_america']
-region = 'southeast_asia'
-N = [1,3,5]
-# N=[3]
-antigenic_resolutions = ['all_effects']#, 'interserotype_effects']
+antigenic_resolutions = ['all_effects', 'interserotype_effects']
 clade_resolutions = ['genotype', 'serotype']
 
-for n in N:
-	for antigenic_resolution in antigenic_resolutions:
-		for clade_resolution in clade_resolutions:
-			frequency_path = './%s/%s/%s_%s_frequencies.csv'%(region, clade_resolution, region, clade_resolution)
-			titer_path = './%s_Dij.csv'%antigenic_resolution
-			out_path = './%s/%s/%s/'%(region, clade_resolution, antigenic_resolution)
-			name = '%s_%s_%s_N%d'%(region, clade_resolution, antigenic_resolution, n)
-			cmd = 'python ./antigenic_fitness.py --frequency_path %s --titer_path %s --out_path %s --name %s --years_back %d --years_forward 1 --beta 0 5 --gamma 0 2 --sigma 0 5 --n_param_vals 15 --plot --save'%(frequency_path, titer_path, out_path, name, n)
-			print cmd
-			os.system("sbatch --time 24:00:00 --wrap='%s'"%cmd)
-
-# python ./antigenic_fitness.py --frequency_path ./flu/flu_frequencies.csv --titer_path ./flu/flu_titers.csv --out_path ./flu/ --name flu --years_back 1 --years_forward 1 --beta 1 15 --gamma -5 0 --sigma -5 0 --n_param_vals 10
+for antigenic_resolution in antigenic_resolutions:
+	for clade_resolution in clade_resolutions:
+		if antigenic_resolution == 'all_effects' and clade_resolution == 'serotype':
+			continue
+		frequency_path = './southeast_asia/%s/southeast_asia_%s_frequencies.csv'%(clade_resolution, clade_resolution)
+		titer_path = './%s_Dij.csv'%antigenic_resolution
+		out_path = './southeast_asia/%s/%s/'%(clade_resolution, antigenic_resolution)
+		name = 'seasia_%s_%s'%(clade_resolution, antigenic_resolution)
+		cmd = 'python ./antigenic_fitness.py --frequency_path %s --titer_path %s --out_path %s --name %s --years_back 2 --years_forward 1 --beta 0 3 --gamma 0 2 --sigma 0 3 --n_param_vals 12 --metric sse --plot --save'%(frequency_path, titer_path, out_path, name)
+		print cmd
+		# os.system("sbatch --wrap='%s'"%cmd)
